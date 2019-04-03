b39daf927dbe4d056bda49011450016b35cf0042
grilledbeer <Monkeychip@users.noreply.github.com>
2019-04-03 14:23:48 -0600

    copying old ronswanson boilerplate over

---
+++ src/components/title.js
@@ -0,0 +1,29 @@
+// @flow
+
+import React, { Component } from "react";
+
+import RonsHead from "../images/ronavatar.jpg";
+import "../App.css";
+
+class Title extends Component<*> {
+
+  render() {
+    return (
+      <div>
+        <span className="ui header center aligned">
+          <div>
+            <img
+              src={RonsHead}
+              className="ui centered aligned circular image medium"
+              id="ron_image"
+              alt=""
+            />
+          </div>
+          <h2>Rate Ron's Quotes</h2>
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Title;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+// @flow
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css'
+
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { store, persistor } from './reduxStore';
+import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+  /* $FlowFixMe */
+  , document.querySelector('#root'));
+  registerServiceWorker();
+
+
+
